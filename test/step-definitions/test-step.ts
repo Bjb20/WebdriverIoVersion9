328bfefaa9d2df7bb060c3ec2aae2adb7c79dcca
Bjb20 <bhaskarbj3@gmail.com>
2026-01-21 12:39:29 +0530

    Add step definition for navigating to Google home page

--- test/step-definitions/test-step.ts
+++ test/step-definitions/test-step.ts
@@ -1,1 +1,6 @@
+import {Given} from "@wdio/cucumber-framework"
+import {simpleTestPage} from "../../test/page-object/test-page-object"
 
+Given(/^I navigate to google home page$/, async () => {
+  await simpleTestPage.navigateToGoogleHome();
+});

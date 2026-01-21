88e18e5949002453f4525a082389e451e033c6a1
Bjb20 <bhaskarbj3@gmail.com>
2026-01-21 12:45:48 +0530

    Create SimpleTestPage for Google navigation
    Added SimpleTestPage class with navigation method.

--- test/page-object/test-page-object.ts
+++ test/page-object/test-page-object.ts
@@ -1,1 +1,11 @@
+export class SimpleTestPage {
+  /**
+   * Navigate to Google home page.
+   * Adjust the URL if you need a localized domain or different URL.
+   */
+  public async navigateToGoogleHome(): Promise<void> {
+    await browser.url("https://www.google.com");
+  }
+}
 
+//export default new SimpleTestPage();
